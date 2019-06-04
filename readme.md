8acca3bd1b2f56b59315674ba3460550f46c6350
Lenin Meza <merolhack@gmail.com>
2019-06-03 19:06:22 -0500

    :sparkles: fix: routes with db queries added

--- readme.md
+++ readme.md
@@ -2,6 +2,15 @@
 
 > Sepomex Postal Codes & Gas Prices API with Slim + MySQL Database
 
+Las rutas de este proyecto se encuentran en:
+
+* routes/web.php
+
+## Documentación
+
+* [Lumen - Installation](https://lumen.laravel.com/docs/5.8)
+* [Laravel - Database: Query Builder](https://laravel.com/docs/5.8/queries)
+
 ## Dependencias
 
 El proyecto ha sido desarrollado con:
@@ -13,4 +22,25 @@ El proyecto ha sido desarrollado con:
 
 ```bash
 composer create-project --prefer-dist laravel/lumen gas-prices-back
+composer require guzzlehttp/guzzle
 ````
+
+Es necesario darle permisos de escritura al siguiente directorio:
+
+```bash
+chmod 777 storage/logs
+````
+
+Es necesario modificar los datos de conexión con la base de datos en el archivo:
+
+* .env
+  * DB_CONNECTION
+  * DB_HOST
+  * DB_PORT
+  * DB_DATABASE
+  * DB_USERNAME
+  * DB_PASSWORD
+
+En ese mismo archivo se debe deshabilitar el modo estricto de MySQL:
+
+* DB_STRICT_MODE=false
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,33 @@
 */
 
 $router->get('/', function () use ($router) {
-    return $router->app->version();
+    return "Sepomex Postal Codes & Gas Prices API with Slim + MySQL Database";
+});
+
+// Gets the gas-prices from the API
+$router->get('/gas-prices', function () {
+    $client = new \GuzzleHttp\Client();
+    $request = $client->get('https://api.datos.gob.mx/v1/precio.gasolina.publico');
+    $response = json_decode($request->getBody(), true);
+    dd($response->results);
+    return $response;
+});
+
+// Gets the states from the database
+$router->get('/states', function () {
+    $results = DB::table('zip-codes')
+        ->select('c_estado AS id', 'd_estado AS name')
+        ->groupBy('zip-codes.c_estado')
+        ->get();
+    return json_encode($results);
+});
+
+// Gets the municipalities from the database
+$router->get('/municipalities/{stateId}', function ($stateId = 01) {
+    $results = DB::table('zip-codes')
+        ->select('c_estado AS state_id', 'c_mnpio AS id', 'd_mnpio AS name')
+        ->where('c_estado', $stateId)
+        ->groupBy('zip-codes.d_mnpio')
+        ->get();
+    return json_encode($results);
 });
